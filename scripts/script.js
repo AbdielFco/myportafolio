400cfab2587f2f143cda1961d11411d6a91f9554
AbdielFco <88990612+AbdielFco@users.noreply.github.com>
2023-09-08 14:49:29 -0400

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,15 @@
+$(document).ready(function() {
+  // Captura el clic en los enlaces de navegación con scroll suave
+  $('a.nav-link').on('click', function(event) {
+    event.preventDefault();
+
+    // Obtiene el atributo href del enlace
+    var target = $(this).attr('href');
+
+    // Realiza el desplazamiento suave hacia el objetivo
+    $('html, body').animate({
+      scrollTop: $(target).offset().top
+    }, 800); // Puedes ajustar la velocidad de la animación aquí
+  });
+});
 
